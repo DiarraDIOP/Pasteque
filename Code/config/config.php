4360b82c1f533782ae39f4ab27996b5ff68f3167
DiarraDIOP <sokhna.d.diop@gmail.com>
2016-02-29 22:46:28 +0100

    fichier php de config BD

--- Code/config/config.php
+++ Code/config/config.php
@@ -1,16 +1,13 @@
 <?php
        
-
-/*$pdo = new PDO('mysql:host=localhost;dbname=bddtest', 'root', '');     */  
 $user='root';
 $pass='';
 $serveur='localhost';
 
-$dernierebase='bddtest';
+$dernierebase='pasteque';
 try
 {
-	$dbh=new PDO("mysql:host=$serveur;dbname=$dernierebase;",$user,$pass);
-
+	$pdo = new PDO("mysql:host=$serveur;dbname=$dernierebase;",$user,$pass); 
 }
 catch(PDOException $e)
 {
