60ce870e75414cf8016af814af4a9bc0695125ed
ILHAM MESTARI <ilham.mestari@gmail.com>
2016-03-06 17:56:58 +0100

    modification de la page de connexion

---
+++ Code/script/connect.php
@@ -0,0 +1,34 @@
+<?php
+if(!isset($_GET['login']) && !isset($_GET['motdepasse']))
+{
+    header('Location: page_connexion.html');
+    Exit;
+}
+else
+{
+
+}
+?>
+<?php
+if(!isset($_GET['login']) && !isset($_GET['motdepasse']))
+{
+    header('Location: page_connexion.html');
+}
+else
+{
+    // On va vérifier les variables
+    if(!preg_match('/^[[:alnum:]]+$/', $_GET['login']) or
+!preg_match('/^[[:alnum:]]+$/', $_GET['motdepasse']))
+    {
+        echo 'Vous devez entrer uniquement des lettres ou des chiffres <br/>';
+        echo '<a href="page_connexion" temp_href="page_connexion.html">Réessayer</a>';
+        exit();
+    }
+    else
+    {
+        require('config.php'); // On réclame le fichier
+        $login = $_GET['login'];
+        $motdepasse = $_GET['motdepasse'];
+    }
+}
+?>
