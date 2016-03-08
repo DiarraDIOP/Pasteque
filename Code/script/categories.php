8e97d5ad6acd9cde5d7a98b69b221bb70c5c32aa
AmalBachara <bachara.amal@gmail.com>
2016-03-08 15:07:19 +0100

    supression du dossier calculatrice qui ne sert plus à rien

--- Code/script/categories.php
+++ Code/script/categories.php
@@ -3,15 +3,20 @@
 require '../config/config.php'; 
 
  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
  $sql="SELECT `ID`, `REFERENCE`, `NAME`, `PARENTID`, `IMAGE`, `DISPORDER` FROM categories";
+
  $data = $pdo->query($sql);
  $data->setFetchMode(PDO::FETCH_ASSOC);
- $categories= array();
+
+/* $categories= array();*/
 
    foreach($data as $row)
 {
-	$category = array('ID' => $row['ID'], 'REFERENCE' => $row['REFERENCE'], 'NAME' => $row['NAME'], 'PARENTID' => $row['PARENTID'], 'IMAGE' => $row['IMAGE'], 'DISPORDER' => $row['DISPORDER']);
-	array_push($categories, $category);
+	/*$category = array('ID' => $row['ID'], 'REFERENCE' => $row['REFERENCE'], 'NAME' => $row['NAME'], 'PARENTID' => $row['PARENTID'], 'IMAGE' => $row['IMAGE'], 'DISPORDER' => $row['DISPORDER']);
+	*/print "<li><a href='#'>". $row['NAME'] ." </a></li>";
 }
+
+
 
 ?>
